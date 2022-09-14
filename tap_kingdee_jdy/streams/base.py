497dae94853fe2892951519eb857d97e20a588d9
LesleyDing <leslietdhz2014@gmail.com>
2022-09-14 14:45:17 +0800

    change replication_method to FULL_TABLE

--- tap_kingdee_jdy/streams/base.py
+++ tap_kingdee_jdy/streams/base.py
@@ -33,7 +33,8 @@ class Base:
 
     @property
     def replication_method(self):
-        return "INCREMENTAL"
+        return "FULL_TABLE"
+        # return "INCREMENTAL"
 
     @property
     def state(self):
@@ -63,10 +64,10 @@ class Base:
         }
         params = {"access_token": config["access_token"]}
 
-        today = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0).isoformat()
-        self._start_date = config.get("start_date", today) # config start date
+        # today = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0).isoformat()
+        # self._start_date = config.get("start_date", today) # config start date
         self._backoff_seconds = config.get("rate_limit_backoff_seconds", DEFAULT_BACKOFF_SECONDS)
-        self._state = state.copy()
+        # self._state = state.copy()
 
         for accountId in config["accountIds"]:
             headers = base_headers.copy()
@@ -74,16 +75,18 @@ class Base:
             yield from self.get_account_data(headers, params)
     
     def get_account_data(self, headers, params):
-        state_date = self._state.get(headers['accountId'], self._start_date) # state start date
-        start = max(parse(self._start_date), parse(state_date))
-        max_rep_key = start
-        LOGGER.info(f"start from {start.isoformat()} for account {headers['accountId']}")
+        # state_date = self._state.get(headers['accountId'], self._start_date) # state start date
+        # start = max(parse(self._start_date), parse(state_date))
+        # max_rep_key = start
+        # LOGGER.info(f"start from {start.isoformat()} for account {headers['accountId']}")
+        LOGGER.info(f"start for account {headers['accountId']}")
         page = 1 
         while True:
             try:
                 resp = requests.post(url=f"{BASE_URL}{self.specific_api}_list",
                     headers=headers, params=params,
-                    json={"pagesize": 100, "page": page, "begindate": start.strftime("%Y-%m-%d")})
+                    json={"pagesize": 100, "page": page})
+                    # json={"pagesize": 100, "page": page, "begindate": start.strftime("%Y-%m-%d")})
                 LOGGER.info(f"{self.specific_api}_list status_code: {resp.status_code}")
                 if resp.status_code == 200:
                     resp = resp.json()
@@ -94,9 +97,9 @@ class Base:
                             data = self.get_detail_data(row["id"], headers, params)
                             if data:
                                 data["accountId"] = headers["accountId"]
-                                rep_key = data.get(self.replication_key)
-                                if rep_key and parse(rep_key) > max_rep_key:
-                                    max_rep_key = parse(rep_key)
+                                # rep_key = data.get(self.replication_key)
+                                # if rep_key and parse(rep_key) > max_rep_key:
+                                #     max_rep_key = parse(rep_key)
                                 yield data
                 
                 page += 1
@@ -105,7 +108,7 @@ class Base:
                 LOGGER.warning(f"{e} Waiting {self._backoff_seconds} seconds...")
                 time.sleep(self._backoff_seconds)
 
-        self._state[headers['accountId']] = max_rep_key.isoformat()
+        # self._state[headers['accountId']] = max_rep_key.isoformat()
 
     def get_detail_data(self, id, headers, params):
         resp = requests.post(url=f"{BASE_URL}{self.specific_api}_detail",
